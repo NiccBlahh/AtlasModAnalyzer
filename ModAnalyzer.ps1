bf975940150b637dae5e2998c226e2f1eb62028f
Niccc <nicknicc2@gmail.com>
2026-06-19 00:59:01 +0300

    Update ModAnalyzer.ps1

--- ModAnalyzer.ps1
+++ ModAnalyzer.ps1
@@ -6,19 +6,24 @@ Clear-Host
 
  $Banner = @"
 
-_____    __   .__                          _____               .___
-  /  _  \ _/  |_ |  |  _____     ______      /     \    ____    __| _/
- /  /_\  \\   __\|  |  \__  \   /  ___/     /  \ /  \  /  _ \  / __ |
-/    |    \|  |  |  |__ / __ \_ \___ \     /    Y    \(  <_> )/ /_/ |
-\____|__  /|__|  |____/(____  //____  >    \____|__  / \____/ \____ |
-        \/                  \/      \/             \/
-
-       _____                   .__
-      /  _  \    ____  _____   |  |  ___.__.________  ____ _______
-     /  /_\  \  /    \ \__  \  |  | <   |  |\___   /_/ __ \\_  __ \
-    /    |    \|   |  \ / __ \_|  |__\___  | /    / \  ___/ |  | \/
-    \____|__  /|___|  /(____  /|____// ____|/_____ \ \___  >|__|
-            \/      \/      \/       \/           \/     \/
+▄████████     ███      ▄█          ▄████████    ▄████████        ▄▄▄▄███▄▄▄▄    ▄██████▄  ████████▄  
+  ███    ███ ▀█████████▄ ███         ███    ███   ███    ███      ▄██▀▀▀███▀▀▀██▄ ███    ███ ███   ▀███ 
+  ███    ███    ▀███▀▀██ ███         ███    ███   ███    █▀       ███   ███   ███ ███    ███ ███    ███ 
+  ███    ███     ███   ▀ ███         ███    ███   ███             ███   ███   ███ ███    ███ ███    ███ 
+▀███████████     ███     ███       ▀███████████ ▀███████████      ███   ███   ███ ███    ███ ███    ███ 
+  ███    ███     ███     ███         ███    ███          ███      ███   ███   ███ ███    ███ ███    ███ 
+  ███    ███     ███     ███▌    ▄   ███    ███    ▄█    ███      ███   ███   ███ ███    ███ ███   ▄███ 
+  ███    █▀     ▄████▀   █████▄▄██   ███    █▀   ▄████████▀        ▀█   ███   █▀   ▀██████▀  ████████▀  
+                         ▀                                                                              
+             ▄████████ ███▄▄▄▄      ▄████████  ▄█       ▄██   ▄    ▄███████▄     ▄████████    ▄████████ 
+            ███    ███ ███▀▀▀██▄   ███    ███ ███       ███   ██▄ ██▀     ▄██   ███    ███   ███    ███ 
+            ███    ███ ███   ███   ███    ███ ███       ███▄▄▄███       ▄███▀   ███    █▀    ███    ███ 
+            ███    ███ ███   ███   ███    ███ ███       ▀▀▀▀▀███  ▀█▀▄███▀▄▄  ▄███▄▄▄      ▄███▄▄▄▄██▀ 
+              ▀███████████ ███   ███ ▀███████████ ███       ▄██   ███   ▄███▀   ▀ ▀▀███▀▀▀     ▀▀███▀▀▀▀   
+            ███    ███ ███   ███   ███    ███ ███       ███   ███ ▄███▀         ███    █▄  ▀███████████ 
+            ███    ███ ███   ███   ███    ███ ███▌    ▄ ███   ███ ███▄     ▄█   ███    ███   ███    ███ 
+            ███    █▀   ▀█   █▀    ███    █▀  █████▄▄██  ▀█████▀   ▀████████▀   ██████████   ███    ███ 
+                                              ▀                                              ███    ███ 
 
 "@
 
@@ -28,19 +33,24 @@ Write-Host ""
 Write-Host ("━" * 76) -ForegroundColor DarkCyan
 Write-Host
 
-# Automatic Path Detection (No Prompt)
-if ($args.Count -gt 0) {
-    $modsPath = $args[0]
-    Write-Host "Scanning provided path: $modsPath" -ForegroundColor DarkGray
-} else {
-    $modsPath = "$env:USERPROFILE\AppData\Roaming\.minecraft\mods"
-    Write-Host "Scanning default path: $modsPath" -ForegroundColor DarkGray
-}
+# Input Logic (Restored)
+Write-Host "Enter path to the mods folder: " -NoNewline
+Write-Host "(press Enter to use default)" -ForegroundColor DarkGray
+ $modsPath = Read-Host "PATH"
 Write-Host
+
+if ([string]::IsNullOrWhiteSpace($modsPath)) {
+    $modsPath = "$env:USERPROFILE\AppData\Roaming\.minecraft\mods"
+    Write-Host "Using default path: $modsPath" -ForegroundColor White
+    Write-Host
+} else {
+    Write-Host "Using custom path: $modsPath" -ForegroundColor White
+    Write-Host
+}
 
 if (-not (Test-Path $modsPath -PathType Container)) {
     Write-Host
-    Write-Host "  ╔═══════════════════════════════════════════════════════════════╗" -ForegroundColor Red
+    Write-Host "  ╔═════════════════════════════════════════════════════════════════╗" -ForegroundColor Red
     Write-Host "  ║ " -NoNewline -ForegroundColor Red
     Write-Host "✖  FATAL ERROR: INVALID PATH" -NoNewline -ForegroundColor White -BackgroundColor Red
     Write-Host (" " * (51 - "✖  FATAL ERROR: INVALID PATH".Length)) -NoNewline
@@ -100,121 +110,41 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
  $cheatStrings = @(
     "AutoCrystal","autocrystal","auto crystal","cw crystal","JDWP.VirtualMachine.AllModules",
     "dontPlaceCrystal","dontBreakCrystal","AutoHitCrystal","autohitcrystal","canPlaceCrystalServer",
-    "healPotSlot","ＡｕｔｏＣｒｙｓｔａｌ","Ａｕｔｏ Ｃｒｙｓｔａｌ","ＡｕｔｏＨｉｔＣｒｙｓｔａｌ",
+    "healPotSlot","ＡｕｔｏＣｒｙｓ","Ａｕｔｏ Ｃｒｙｓ","ＡｕｔｏＨｉｈｏｃ","ＡｕｔｏＨｉｈｏｃ",
     "AutoAnchor","autoanchor","auto anchor","DoubleAnchor","HasAnchor","anchortweaks","anchor macro",
-    "safe anchor","safeanchor","SafeAnchor","AirAnchor","ＡｕｔｏＡｎｃｈｏｒ","Ａｕｔｏ Ａｎｃｈｏｒ",
-    "ＤｏｕｂｌｅＡｎｃｈｏｒ","Ｄｏｕｂｌｅ Ａｎｃｈｏｒ","ＳａｆｅＡｎｃｈｏｒ","Ｓａｆｅ Ａｎｃｈｏｒ",
-    "Ａｎｃｈｏｒ Ｍａｃｒｏ","anchorMacro","AutoTotem","autototem","auto totem","InventoryTotem",
-    "inventorytotem","HoverTotem","hover totem","legittotem","ＡｕｔｏＴｏｔｅｍ","Ａｕｔｏ Ｔｏｔｅｍ",
-    "ＨｏｖｅｒＴｏｔｅｍ","Ｈｏｖｅｒ Ｔｏｔｅｍ","ＩｎｖｅｎｔｏｒｙＴｏｔｅｍ",
-    "Ａｕｔｏ Ｉｎｖｅｎｔｏｒｙ Ｔｏｔｅｍ","Ａｕｔｏ Ｔｏｔｅｍ Ｈｉｔ",
+    "safe anchor","safeanchor","SafeAnchor","AirAnchor","Ａｕｔｏ｡ｎｃｈ","Ａｕｔｏ Ａｎｃｈ",
+    "ＤｏｕｂｌｅＡｎ","Ｄｏｕｂｌｅ Ａｎｃｈ","Ｓａｆｅｌ＋ｓ","Ｓａｆｅ",
+    "AutoTotem","autototem","auto totem","InventoryTotem",
+    "inventorytotem","HoverTotem","hover totem","legittotem","ＡｕｔｏＴｏｔｍ","Ａｕｔｏ Ｔｏｔｍ",
+    "Ｈｏｖｅ▒ﾝ","Ｈｏｖｅ ▀█","Ｈｏｖｅ ▀█","Ｉｎｖｅｎｔｏｒｙ","Ｉｎｖｅｎ╔Ｓｗｏ▒",
     "AutoPot","autopot","auto pot","speedPotSlot","strengthPotSlot","AutoArmor","autoarmor","auto armor",
-    "ＡｕｔｏＰｏｔ","Ａｕｔｏ Ｐｏｔ","Ａｕｔｏ Ｐｏｔ Ｒｅｆｉｌｌ","AutoPotRefill",
-    "ＡｕｔｏＡｒｍｏｒ","Ａｕｔｏ Ａｒｍｏｒ","preventSwordBlockBreaking","preventSwordBlockAttack",
-    "ShieldDisabler","ShieldBreaker","ＳｈｉｅｌｄＤｉｓａｂｌｅｒ","Ｓｈｉｅｌｄ Ｄｉｓａｂｌｅｒ",
-    "Breaking shield with axe...","AutoDoubleHand","autodoublehand","auto double hand",
-    "ＡｕｔｏＤｏｕｂｌｅＨａｎｄ","Ａｕｔｏ Ｄｏｕｂｌｅ Ｈａｎｄ","AutoClicker","ＡｕｔｏＣｌｉｃｋｅｒ",
-    "Failed to switch to mace after axe!","AutoMace","MaceSwap","SpearSwap","ＡｕｔｏＭａｃｅ",
-    "Ａｕｔｏ Ｍａｃｅ","ＭａｃｅＳｗａｐ","Ｍａｃｅ Ｓｗａｐ","Ｓｐｅａｒ Ｓｗａｐ",
-    "Ａｕｔｏｍａｔｉｃａｌｌｙ ａｘｅ ａｎｄ ｍａｃｅ ｓｈｉｅｌｄｅｄ ｐｌａｙｅｒｓ",
+    "Ａｕｔｏ","Ａｕｔｏ","Ａｕｔｏ","Ｌｅｆｉｌ","AutoPotRefill",
+    "Ａｕｏ","Ａｕｔ","Ａｕｔ","ｐｒｅｅｅｎｔ","Ａｕｔｏ Ａｒｍｏｒ",
+    "Ｓｈｉｅｌ","Ｓｈｉｅ","Breaking shield with axe...","AutoDoubleHand","autodoublehand","auto double hand",
+    "Ａｕｔｏ","Ａｕｔ","Ａｕｔ","Ａｕｔ","AutoClicker","Ａｕｔｏ｣ｌｉｃｋ",
+    "Failed to switch to mace after axe!","AutoMace","MaceSwap","SpearSwap","Ａｕｔｏ","Ｍａｃｅ",
+    "Ａｕｏ","Ｍａｃ","Ｍａｃｅ","Ｓｗａｐ","Ｍａｃ",
+    "Ａｕｔｏｍａｔｉｃａｌｌｙ ａｘｅ ａｎｄ ｍａｃｅ ｓ",
     "Ｓｔｕｎ Ｓｌａｍ","StunSlam","Donut","JumpReset","axespam","axe spam",
     "findKnockbackSword","attackRegisteredThisClick","AimAssist","aimassist","aim assist",
-    "triggerbot","trigger bot","ＡｉｍＡｓｓｉｓｔ","Ａｉｍ Ａｓｓｉｓｔ","ＴｒｉｇｇｅｒＢｏｔ","Ｔｒｉｇｇｅｒ Ｂｏｔ",
-    "Silent Rotations","SilentRotations","Ｓｉｌｅｎｔ Ｒｏｔａｔｉｏｎｓ","FakeInv","swapBackToOriginalSlot",
-    "FakeLag","pingspoof","ping spoof","ＦａｋｅＬａｇ","Ｆａｋｅ Ｌａｇ","fakePunch","Fake Punch","Ｆａｋｅ Ｐｕｎｃｈ",
+    "triggerbot","trigger bot","Ａｉｍ","Ａｉ Ａｓｉｓ","Ｔｒｉｇｇ",
+    "Silent Rotations","SilentRotations","Ｓｉｌｅ","Ｓｉｌ","FakeInv","swapBackToOriginalSlot",
+    "FakeLag","pingspoof","ping spoof","Ｆａｋｅ","Ｆａｋｅ","fakePunch","Fake Punch","Ｆａｋ",
     "mace_swap","quick_strike","macro_198","stun_slam","safe_anchor","double_anchor","auto_pot_refill",
     "walksy_optimizer","key_pearl","aim_assist","auto_neth_pot","auto_dtap","trigger_bot","auto_web",
     "DOUBLE_ESCAPE","DOUBLE_RIGHTCLICK_FIRST","DOUBLE_RIGHTCLICK_SECOND","POST_CYCLE_DELAY",
     "PLACE_OBI","WAIT_OBI","PLACE_CRYSTAL","BREAK_CRYSTAL","ROTATING_DOWN","ROTATING_BACK",
     "REFILLING","PLANTING","BONEMEALING","AnchorAction","Places two anchors for massive damage",
-    "REOFFHAND_TOTEM","webmacro","web macro","AntiWeb","AutoWeb","Ａｎｔｉ Ｗｅｂ","ＡｕｔｏＷｅｂ",
-    "Ｐｌａｃｅｓ Ｗｅｂｓ Ｏｎ Ｅｎｅｍｉｅｓ","lvstrng","dqrkis","selfdestruct","self destruct",
-    "WalksyCrystalOptimizerMod","WalksyOptimizer","WalskyOptimizer","Ｗａｌｋｓｙ Ｏｐｔｉｍｉｚｅｒ",
+    "REOFFHAND_TOTEM","webmacro","web macro","AntiWeb","AutoWeb","Ａｎｔﾉ Ｗｅ",
+    "Ｐｌａｃｅｓ Ｗｅｂｓﾯｎ Ｅｎｅｍｉｅｓ","lvstrng","dqrkis","selfdestruct","self destruct",
+    "WalksyCrystalOptimizerMod","WalksyOptimizer","WalskyOptimizer","Ｗａｌｋｓﾙ",
     "autoCrystalPlaceClock","AutoFirework","ElytraSwap","FastXP","FastExp","NoJumpDelay",
-    "ＥｌｙｔｒａＳｗａｐ","Ｅｌｙｔｒａ Ｓｗａｐ","PackSpoof","Antiknockback","catlean",
+    "Ｅｌｙｔｒａ","Ｅｌｙｔｒａ","PackSpoof","Antiknockback","catlean",
     "AuthBypass","obfuscatedAuth","LicenseCheckMixin","BaseFinder","invsee","ItemExploit",
-    "FreezePlayer","VirtualMachine","Ｆｒｅｅｃａｍ","Ｍｏｖｅ ｆｒｅｅｌｙ ｔｈｒｏｕｇｈ ｗａｌｌｓ",
-    "Ｎｏ Ｃｌｉｐ","Ｆｒｅｅｚｅ Ｐｌａｙｅｒ","LWFH Crystal","JDWP.VirtualMachine.AllModules",
-    "ＬＷＦＨ Ｃｒｙｓｔａｌ","KeyPearl","LootYeeter","ＫｅｙＰｅａｒｌ","Ｋｅｙ Ｐｅａｒｌ","Ｌｏｏｔ Ｙｅｅｔｅｒ",
-    "FastPlace","Ｆａｓｔ Ｐｌａｃｅ","Ｐｌａｃｅ ｂｌｏｃｋｓ ｆａｓｔｅｒ","AutoBreach","Ａｕｔｏ Ｂｒｅａｃｈ",
-    "setBlockBreakingCooldown","getBlockBreakingCooldown","blockBreakingCooldown","onBlockBreaking",
-    "setItemUseCooldown","setSelectedSlot","invokeDoAttack","invokeDoItemUse","invokeOnMouseButton",
-    "onPushOutOfBlocks","onIsGlowing","Automatically switches to sword when hitting with totem",
-    "arrayOfString","POT_CHEATS","Dqrkis Client","Entity.isGlowing","Activate Key","Ａｃｔｉｖａｔｅ Ｋｅｙ",
-    "Click Simulation","Ｃｌｉｃｋ Ｓｉｍｕｌａｔｉｏｎ","On RMB","Ｏｎ ＲＭＢ",
-    "No Count Glitch","Ｎｏ Ｃｏｕｎｔ Ｇｌｉｔｃｈ","No Bounce","NoBounce","Ｎｏ Ｂｏｕｎｃｅ","ＮｏＢｏｕｎｃｅ",
-    "Ｒｅｍｏｖｅｓ ｔｈｅ ｃｒｙｓｔａｌ ｂｏｕｎｃｅ ａｎｉｍａｔｉｏｎ",
-    "Place Delay","Ｐｌａｃｅ Ｄｅｌａｙ","Break Delay","Ｂｒｅａｋ Ｄｅｌａｙ","Ｆａｓｔ Ｍｏｄｅ",
-    "Place Chance","Ｐｌａｃｅ Ｃｈａｎｃｅ","Break Chance","Ｂｒｅａｋ Ｃｈａｎｃｅ",
-    "Stop On Kill","Ｓｔｏｐ Ｏｎ Ｋｉｌｌ","Ｄａｍａｇｅ Ｔｉｃｋ","damagetick",
-    "Anti Weakness","Ａｎｔｉ Ｗｅａｋｎｅｓｓ","Particle Chance","Ｐａｒｔｉｃｌｅ Ｃｈａｎｃｅ",
-    "Trigger Key","Ｔｒｉｇｇｅｒ Ｋｅｙ","Switch Delay","Ｓｗｉｔｃｈ Ｄｅｌａｙ",
-    "Totem Slot","Ｔｏｔｅｍ Ｓｌｏｔ","Silent Rotations","Ｓｉｌｅｎｔ Ｒｏｔａｔｉｏｎｓ",
-    "Smooth Rotations","Ｓｍｏｏｔｈ Ｒｏｔａｔｉｏｎｓ","Rotation Speed","Ｒｏｔａｔｉｏｎ Ｓｐｅｅｄ",
-    "Use Easing","Ｕｓｅ Ｅａｓｉｎｇ","Easing Strength","Ｅａｓｉｎｇ Ｓｔｒｅｎｇｔｈ",
-    "While Use","Ｗｈｉｌｅ Ｕｓｅ","Stop on Kill","Ｓｔｏｐ ｏｎ Ｋｉｌｌ",
-    "Glowstone Delay","Ｇｌｏｗｓｔｏｎｅ Ｄｅｌａｙ","Glowstone Chance","Ｇｌｏｗｓｔｏｎｅ Ｃｈａｎｃｅ",
-    "Explode Delay","Ｅｘｐｌｏｄｅ Ｄｅｌａｙ","Explode Chance","Ｅｘｐｌｏｄｅ Ｃｈａｎｃｅ",
-    "Explode Slot","Ｅｘｐｌｏｄｅ Ｓｌｏｔ","Only Charge","Ｏｎｌｙ Ｃｈａｒｇｅ",
-    "Anchor Macro","Ａｎｃｈｏｒ Ｍａｃｒｏ","Reach Distance","Ｒｅａｃｈ Ｄｉｓｔａｎｃｅ",
-    "Min Height","Ｍｉｎ Ｈｅｉｇｈｔ","Min Fall Speed","Ｍｉｎ Ｆａｌｌ Ｓｐｅｅｄ",
-    "Attack Delay","Ａｔｔａｃｋ Ｄｅｌａｙ","Breach Delay","Ｂｒｅａｃｈ Ｄｅｌａｙ",
-    "Require Elytra","Ｒｅｑｕｉｒｅ Ｅｌｙｔｒａ","Auto Switch Back","Ａｕｔｏ Ｓｗｉｔｃｈ Ｂａｃｋ",
-    "Check Line of Sight","Ｃｈｅｃｋ Ｌｉｎｅ ｏｆ Ｓｉｇｈｔ","Only When Falling","Ｏｎｌｙ Ｗｈｅｎ Ｆａｌｌｉｎｇ",
-    "Require Crit","Ｒｅｑｕｉｒｅ Ｃｒｉｔ","Show Status Display","Ｓｈｏｗ Ｓｔａｔｕｓ Ｄｉｓｐｌａｙ",
-    "Stop On Crystal","Ｓｔｏｐ Ｏｎ Ｃｒｙｓｔａｌ","Check Shield","Ｃｈｅｃｋ Ｓｈｉｅｌｄ",
-    "On Pop","Ｏｎ Ｐｏｐ","Predict Damage","Ｐｒｅｄｉｃｔ Ｄａｍａｇｅ",
-    "On Ground","Ｏｎ Ｇｒｏｕｎｄ","Check Players","Ｃｈｅｃｋ Ｐｌａｙｅｒｓ",
-    "Predict Crystals","Ｐｒｅｄｉｃｔ Ｃｒｙｓｔａｌｓ","Check Aim","Ｃｈｅｃｋ Ａｉｍ",
-    "Check Items","Ｃｈｅｃｋ Ｉｔｅｍｓ","Activates Above","Ａｃｔｉｖａｔｅｓ Ａｂｏｖｅ",
-    "Blatant","Ｂｌａｔａｎｔ","Force Totem","Ｆｏｒｃｅ Ｔｏｔｅｍ",
-    "Stay Open For","Ｓｔａｙ Ｏｐｅｎ Ｆｏｒ","Auto Inventory Totem","Ａｕｔｏ Ｉｎｖｅｎｔｏｒｙ Ｔｏｔｅｍ",
-    "Only On Pop","Ｏｎｌｙ Ｏｎ Ｐｏｐ","Vertical Speed","Ｖｅｒｔｉｃａｌ Ｓｐｅｅｄ",
-    "Hover Totem","Ｈｏｖｅｒ Ｔｏｔｅｍ","Swap Speed","Ｓｗａｐ Ｓｐｅｅｄ",
-    "Strict One-Tick","Ｓｔｒｉｃｔ Ｏｎｅ－Ｔｉｃｋ","Mace Priority","Ｍａｃｅ Ｐｒｉｏｒｉｔｙ",
-    "Min Totems","Ｍｉｎ Ｔｏｔｅｍｓ","Min Pearls","Ｍｉｎ Ｐｅａｒｌｓ",
-    "Totem First","Ｔｏｔｅｍ Ｆｉｒｓｔ","Drop Interval","Ｄｒｏｐ Ｉｎｔｅｒｖａｌ",
-    "Random Pattern","Ｒａｎｄｏｍ Ｐａｔｔｅｒｎ","Loot Yeeter","Ｌｏｏｔ Ｙｅｅｔｅｒ",
-    "Horizontal Aim Speed","Ｈｏｒｉｚｏｎｔａｌ Ａｉｍ Ｓｐｅｅｄ","Vertical Aim Speed","Ｖｅｒｔｉｃａｌ Ａｉｍ Ｓｐｅｅｄ",
-    "Include Head","Ｉｎｃｌｕｄｅ Ｈｅａｄ","Web Delay","Ｗｅｂ Ｄｅｌａｙ",
-    "Holding Web","Ｈｏｌｄｉｎｇ Ｗｅｂ","Not When Affects Player","Ｎｏｔ Ｗｈｅｎ Ａｆｆｅｃｔｓ Ｐｌａｙｅｒ",
-    "Hit Delay","Ｈｉｔ Ｄｅｌａｙ","Ｓｗｉｔｃｈ Ｂａｃｋ","Require Hold Axe","Ｒｅｑｕｉｒｅ Ｈｏｌｄ Ａｘｅ",
-    "Fake Punch","Ｆａｋｅ Ｐｕｎｃｈ","placeInterval","breakInterval","stopOnKill",
-    "activateOnRightClick","holdCrystal","ｐｌａｃｅＩｎｔｅｒｖａｌ","ｂｒｅａｋＩｎｔｅｒｖａｌ",
-    "ｓｔｏｐＯｎＫｉｌｌ","ａｃｔｉｖａｔｅＯｎＲｉｇｈｔＣｌｉｃｋ","ｄａｍａｇｅｔｉｃｋ",
-    "ｈｏｌｄＣｒｙｓｔａｌ","ｆａｋｅＰｕｎｃｈ",
-    "Ｒｅｆｉｌｌｓ ｙｏｕｒ ｈｏｔｂａｒ ｗｉｔｈ ｐｏｔｉｏｎｓ",
-    "Ｋｅｐｓ ｙｏｕ ｓｐｒｉｎｔｉｎｇ ａｔ ａｌｌ ｔｉｍｅｓ",
-    "Ｐｌａｃｅｓ ａｎｃｈｏｒ， ｃｈａｒｇｅｓ ｉｔ， ｐｒｏｔｅｃｔｓ ｙｏｕ， ａｎｄ ｅｘｐｌｏｄｅｓ",
-    "Ａｕｔｏ ｓｗａｐ ｔｏ ｓｐｅａｒ ｏｎ ａｔｔａｃｋ","Macro Key","Ａｕｔｏ Ｐｏｔ","Ｍａｃｒｏ Ｋｅｙ",
-    "KillAura","ClickAura","MultiAura","ForceField","LegitAura","AimBot","AutoAim","SilentAim",
-    "AimLock","HeadSnap","CrystalAura","AnchorAura","AnchorFill","AnchorPlace","BedAura","AutoBed",
-    "BedBomb","BedPlace","BowAimbot","BowSpam","AutoBow","AutoCrit","CritBypass","AlwaysCrit",
-    "CriticalHit","ReachHack","ExtendReach","LongReach","HitboxExpand","AntiKB","NoKnockback",
-    "GrimVelocity","GrimDisabler","VelocitySpoof","KBReduce","OffhandTotem","TotemSwitch",
-    "AutoWeapon","AutoSword","AutoCity","Burrow","SelfTrap","HoleFiller","AntiSurround","AntiBurrow",
-    "WTap","TargetStrafe","AutoGap","AutoPearl","FlyHack","CreativeFlight","BoatFly","PacketFly",
-    "AirJump","SpeedHack","BHop","BunnyHop","AntiFall","NoFallDamage","SafeFall","StepHack",
-    "FastClimb","AutoStep","HighStep","WaterWalk","LiquidWalk","LavaWalk","NoSlow","NoSlowdown",
-    "NoWeb","NoSoulSand","WallHack","ElytraSpeed","InstantElytra","ScaffoldWalk","FastBridge",
-    "BuildHelper","AutoBridge","Nuker","NukerLegit","InstantBreak","GhostHand","NoSwing",
-    "PlaceAssist","AirPlace","AutoPlace","InstantPlace","PlayerESP","MobESP","ItemESP","StorageESP",
-    "ChestESP","Tracers","NameTagsHack","XRayHack","OreFinder","CaveFinder","OreESP","NewChunks",
-    "ChunkBorders","TunnelFinder","TargetHUD","ReachDisplay","DoubleClicker","JitterClick",
-    "ButterflyClick","CPSBoost","ChestStealer","InvManager","InvMovebypass","AutoSprint","AntiAFK",
-    "AutoRespawn","FakeNick","PopSwitch","FakeLatency","FakePing","SpoofRotation","PositionSpoof",
-    "GameSpeed","SpeedTimer","GrimBypass","VulcanBypass","MatrixBypass","AACBypass","VerusDisabler",
-    "IntaveBypass","WatchdogBypass","PacketMine","PacketWalk","PacketSneak","PacketCancel",
-    "PacketDupe","PacketSpam","SelfDestruct","HideClient","SessionStealer","TokenLogger",
-    "TokenGrabber","DiscordToken","RemoteAccess","ReverseShell","C2Server","Backdoor","KeyLogger",
-    "StashFinder","TrailFinder","imgui.binding","JNativeHook","GlobalScreen","NativeKeyListener",
-    "client-refmap.json","cheat-refmap.json",
-    "aHR0cDovL2FwaS5ub3ZhY2xpZW50LmxvbC93ZWJob29rLnR4dA==",
-    "meteordevelopment","cc/novoline","com/alan/clients","club/maxstats","wtf/moonlight",
-    "me/zeroeightsix/kami","net/ccbluex","today/opai","net/minecraft/injection",
-    "org/chainlibs/module/impl/modules","xyz/greaj","com/cheatbreaker","com/moonsworth",
-    "doomsdayclient","DoomsdayClient","doomsday.jar","novaclient","api.novaclient.lol",
-    "WalksyOptimizer","LWFH Crystal","vape.gg","vapeclient","VapeClient","VapeLite",
+    "FreezePlayer","VirtualMachine","Ｆ▒ｅｅｃﾁ",
+    "Ｎｏ Ｃﾌﾉｐ","Ｆｒｅﾚ",
+    "ＬＷﾦｨＣｒｙｓｔ","LWFH Crystal","JDWP.VirtualMachine.AllModules",
+    "ＬＷﾦＨ","Ｌｏｏ",
     "intent.store","IntentClient","rise.today","riseclient.com","meteor-client","meteorclient",
     "meteordevelopment.meteorclient","liquidbounce","fdp-client","net.ccbluex","novoware",
     "novoclient","aristois","impactclient","azura","pandaware","skilled","moonClient","astolfo",
@@ -467,7 +397,7 @@ function Invoke-JvmScan {
             "cheatClientBrand"='-D(client|launcher)\.brand=(Wurst|Aristois|Impact|Kilo|Future|Lambda|Rusher|Konas|Phobos|Salhack|ForgeHax|Mathax|Meteor|Async|Seppuku|Xatz|Wolfram|Huzuni|Jigsaw|Zamorozka|Moon|Rage|Exhibition|Virtue|Novoline|Rekt|Skid|Ares|Abyss|Thunder|Tenacity|Rise|Flux|Gamesense|Intent|Remix|Sight|Vape|Shield|Ghost|Crispy|Inertia)';
             "optifine"='-Doptifine\.'; "shadersmod"='-Dshaders?\.'; "shaderPack"='-Dshader[sP]ack='; "cheatPattern"='-D(xray|fly|speed|killaura|reach|esp|wallhack|noclip|autoclick|aimbot|triggerbot|antiknockback|nofall|timer|step|fullbright|nightvision|cavefinder)\.'
         }
-        $cheatClients = @('Wurst','Aristois','Impact','Kilo','Future','Lambda','Rusher','Konas','Phobos','Salhack','ForgeHax','Mathax','Meteor','Async','Seppuku','Xatz','Wolfram','Huzuni','Jigsaw','Zamorozka','Moon','Rage','Exhibition','Virtue','Novoline','Rekt','Skid','Ares','Abyss','Thunder','Tenacity','Rise','Flux','Gamesense','Intent','Remix','Sight','Vape','Shield','Ghost','Crispy','Inertia')
+        $cheatClients = @('Wurst','Aristois','Impact','Kilo','Future','Lambda','Rusher','Konas','Phobos','Salhack','ForgeHax','Mathax','Meteor','Async','Seppuku','Xatz','Wolfram','Huzuni','Jigsaw','Zamorozka','Moon','Rage','Exhibition','Virtue','Novoline','Rekt','Skid','Ares','Abyss','Thunder','Tenacity','Rise','Flux','Gamesense|Intent|Remix|Sight|Vape','Shield','Ghost','Crispy','Inertia')
 
         foreach ($proc in $javaProcesses) {
             try {
